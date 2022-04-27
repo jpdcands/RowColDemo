1169192677845b471e46906aac0180dd91942580
Paul <jpdcands211@gmail.com>
2022-04-27 16:14:42 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.rowcoldemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Column() {
+        Row(
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.size(width = 800.dp, height = 600.dp)) {
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = {},
+                    Modifier.padding(20.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    border = BorderStroke(3.dp, Color.Black),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.Red
+                    )
+                ) {
+                    Text(text = "Upper Body")
+                }
+                Button(
+                    onClick = {},
+                    Modifier.padding(20.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    border = BorderStroke(3.dp, Color.Black),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.Red
+                    )
+                ) {
+                    Text(text = "Abs")
+                }
+                Button(
+                    onClick = {},
+                    Modifier.padding(20.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    border = BorderStroke(3.dp, Color.Black),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.Red
+                    )
+                ) {
+                    Text(text = "Balance")
+                }
+            }
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = {},
+                    Modifier.padding(20.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    border = BorderStroke(3.dp, Color.Black),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.Red
+                    )
+                ) {
+                    Text(text = "Lower Body")
+                }
+                Button(
+                    onClick = {},
+                    Modifier.padding(20.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    border = BorderStroke(3.dp, Color.Black),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.Red
+                    )
+                ) {
+                    Text(text = "Cardio")
+                }
+                Button(
+                    onClick = {},
+                    Modifier.padding(20.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    border = BorderStroke(3.dp, Color.Black),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color.Red
+                    )
+                ) {
+                    Text(text = "Other ")
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainScreen()
+}
