b6307d8c0a2121e6cdac4d7608318bea4c8d6500
Paul <jpdcands211@gmail.com>
2022-05-03 13:46:08 -0400

    Took out top column

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -4,26 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.rowcoldemo.ui.theme.RowColDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             Surface(
-                color = MaterialTheme.colors.primarySurface
+            //    color = MaterialTheme.colors.primarySurface,
+                color = Color.Green,
+                modifier = Modifier.fillMaxSize()
             ) {
                 MainScreen()
 
@@ -35,102 +36,94 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Column() {
-        Row(
-            verticalAlignment = Alignment.Bottom,
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.size(width = 800.dp, height = 600.dp)) {
+     Row(
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.size(width = 800.dp, height = 600.dp)) {
 
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Button(
-                    onClick = {},
-                    Modifier.padding(20.dp),
-                    shape = RoundedCornerShape(20.dp),
-                    border = BorderStroke(3.dp, Color.Black),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        backgroundColor = Color.Red
+            Button(
+                onClick = {},
+                Modifier.padding(20.dp),
+                shape = RoundedCornerShape(20.dp),
+                border = BorderStroke(3.dp, Color.Black),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    backgroundColor = Color.Red
                     )
                 ) {
-                    Text(text = "UpperBody")
+                Text(text = "UpperBody")
                 }
-                Button(
-                    onClick = {},
-                    Modifier.padding(20.dp),
-                    shape = RoundedCornerShape(20.dp),
-                    border = BorderStroke(3.dp, Color.Black),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        backgroundColor = Color.Red
+            Button(
+                onClick = {},
+                Modifier.padding(20.dp),
+                shape = RoundedCornerShape(20.dp),
+                border = BorderStroke(3.dp, Color.Black),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    backgroundColor = Color.Red
                     )
                 ) {
-                    Text(text = "Abs")
+                Text(text = "Abs")
                 }
-                Button(
-                    onClick = {},
-                    Modifier.padding(20.dp),
-                    shape = RoundedCornerShape(20.dp),
-                    border = BorderStroke(3.dp, Color.Black),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        backgroundColor = Color.Red
+            Button(
+                onClick = {},
+                Modifier.padding(20.dp),
+                shape = RoundedCornerShape(20.dp),
+                border = BorderStroke(3.dp, Color.Black),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    backgroundColor = Color.Red
                     )
                 ) {
-                    Text(text = "Balance")
+                Text(text = "Balance")
                 }
             }
 
-            Column(
+        Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Button(
-                    onClick = {},
-                    Modifier.padding(20.dp),
-                    shape = RoundedCornerShape(20.dp),
-                    border = BorderStroke(3.dp, Color.Black),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        backgroundColor = Color.Red
+            Button(
+                onClick = {},
+                Modifier.padding(20.dp),
+                shape = RoundedCornerShape(20.dp),
+                border = BorderStroke(3.dp, Color.Black),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    backgroundColor = Color.Red
                     )
                 ) {
-                    Text(text = "Lower Body")
+                Text(text = "Lower Body")
                 }
-                Button(
-                    onClick = {},
-                    Modifier.padding(20.dp),
-                    shape = RoundedCornerShape(20.dp),
-                    border = BorderStroke(3.dp, Color.Black),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        backgroundColor = Color.Red
+            Button(
+                onClick = {},
+                Modifier.padding(20.dp),
+                shape = RoundedCornerShape(20.dp),
+                border = BorderStroke(3.dp, Color.Black),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    backgroundColor = Color.Red
                     )
                 ) {
-                    Text(text = "Cardio")
+                Text(text = "Cardio")
                 }
-                Button(
-                    onClick = {},
-                    Modifier.padding(20.dp),
-                    shape = RoundedCornerShape(20.dp),
-                    border = BorderStroke(3.dp, Color.Black),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = Color.Black,
-                        backgroundColor = Color.Red
+            Button(
+                onClick = {},
+                Modifier.padding(20.dp),
+                shape = RoundedCornerShape(20.dp),
+                border = BorderStroke(3.dp, Color.Black),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    backgroundColor = Color.Red
                     )
                 ) {
-                    Text(text = "Other ")
+                Text(text = "Other ")
                 }
             }
         }
     }
-}
-
-
-
-
-
-
 
 @Preview(showBackground = true)
 @Composable
